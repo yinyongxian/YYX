ad4a665b116bf243bbfa6e51ebaba8ed2016c60a
尹永贤 <yinyongxian@qq.com>
2018-08-27 06:28:43 +0800

    PathHelper

---
+++ YYX.CSharp/YYX.CSharp/Helpers/PathHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace YYX.CSharp.Helpers
+{
+    public static class PathHelper
+    {
+        public static string MapPath(string path)
+        {
+            return Path.Combine(Application.StartupPath, path);
+        }
+    }
+}
